a2ea314706c764183d06444df3c702fb40b22e98
Alexander North <alexandernorth@users.noreply.github.com>
2025-10-01 19:13:29 +0200

    added context wrapped loggers with slog as default implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sdcio/logger
+
+go 1.25.0
+
+require github.com/go-logr/logr v1.4.3
---
+++ logger.go
@@ -0,0 +1,40 @@
+package logger
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/go-logr/logr"
+)
+
+const (
+	VTrace = -8
+	VDebug = int(slog.LevelDebug)
+	VWarn  = int(slog.LevelWarn)
+	VError = int(slog.LevelError)
+)
+
+var fallbackLogger logr.Logger
+var DefaultLogger logr.Logger
+
+func init() {
+	DefaultLogger = logr.FromSlogHandler(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.Level(VTrace)}))
+	fallbackLogger = DefaultLogger.WithName("fallback-logger")
+}
+
+func SetDefaultLogger(logger logr.Logger) {
+	DefaultLogger = logger
+}
+
+func FromContext(ctx context.Context) logr.Logger {
+	log := fallbackLogger
+	if cLog, err := logr.FromContext(ctx); err == nil {
+		log = cLog
+	}
+	return log
+}
+
+func IntoContext(ctx context.Context, logger logr.Logger) context.Context {
+	return logr.NewContext(ctx, logger)
+}
